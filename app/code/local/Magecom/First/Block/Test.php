9368f01c35dafcd0898c7d39775ad1e7aab35cf4
julia <julia@julia-N61PB-M2S.(none)>
2015-10-07 17:23:48 +0300

    homework

---
+++ app/code/local/Magecom/First/Block/Test.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alice
+ * Date: 10/5/15
+ * Time: 9:18 PM
+ */
+class Magecom_First_Block_Test extends  Mage_Core_Block_Template {
+    public  function  TestFunc() {
+        return 'Hello World';
+    }
+}
